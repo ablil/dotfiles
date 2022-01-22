180f3a23f2f507f81d3ead34c713d7148858a7f2
ablil <ablil@protonmail.com>
2022-01-22 14:29:23 +0100

    create .zprofile

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -6,6 +6,7 @@ ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
 ln -sf "$DOTFILES/zsh/.exports" "$HOME/.exports"
 ln -sf "$DOTFILES/zsh/.aliases" "$HOME/.aliases"
 ln -sf "$DOTFILES/zsh/.functions" "$HOME/.functions"
+ln -sf "$DOTFILES/zsh/.zprofile" "$HOME/.zprofile"
 
 # private env variables
 touch "$HOME/.exports_private"
