16703fba8252b93286846b32b59ee018ab91b743
ablil <ablil@protonmail.com>
2022-06-15 20:02:05 +0100

    add new intellij idea settings

--- IntellijIdea/install.sh
+++ IntellijIdea/install.sh
@@ -2,5 +2,10 @@
 
 [[ -z $DOTFILES ]] && echo "Variable \$DOTFILES is not set && exit 1"
 [[ -z $1 ]] && echo "bash $0 /home/<user>/.config/JetBrains/InelljiXXX/" && exit 1
+
 ln -sf "$DOTFILES/IntellijIdea/templates" "$1/templates"
 ln -sf "$DOTFILES/IntellijIdea/.ideavimrc" "$HOME/.ideavimrc"
+
+for file in "$DOTFILES"/IntellijIdea/options/*; do
+  ln -sf "$file" "$1/options/"
+done
