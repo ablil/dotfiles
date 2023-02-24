31956c1d2680670210ce9b9a01f546c8af742786
ablil <ablil@protonmail.com>
2023-02-24 21:39:18 +0100

    install vscode automatically

--- Code/install.sh
+++ Code/install.sh
@@ -8,11 +8,20 @@ elif [[ ! -d $HOME/.config/Code ]]; then
   echo "$HOME/.config/Code is missing, make sure vscode is installed!"
   exit 2;
 else
-  # link config folder
-  ln -sf "$DOTFILES/Code/*" "$HOME/.config/Code/"
+# install vscode
+  sudo apt-get install wget gpg
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/packages.microsoft.gpg
+  sudo install -D -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  sudo apt update && sudo apt install -y code
+  code && pkill code # open and close immediately, so config folder is created
+
+  # link config
+  ln -sf -t $HOME/.config/Code/User $DOTFILES/Code/User/*.json
+  ln -sf -t $HOME/.config/Code/User/snippets $DOTFILES/Code/User/snippets*.json
 
   # install extensions
-  grep -v '^ *#' < extensions.txt | while IFS= read -r line
+  grep -v '^ *#' < $DOTFILES/Code/extensions.txt | while IFS= read -r line
   do
     code --force --install-extension "$line"
   done
