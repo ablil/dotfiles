c14141c4c888322411abce6bbd742449f1ef310f
khalil <khalil.abdelhamid@imedia24.de>
2023-05-01 14:40:10 +0100

    configure custom commands

--- run.sh
+++ run.sh
@@ -13,6 +13,15 @@ ln -sf $DOTFILES/.vimrc $HOME/.vimrc
 
 ln -sf $DOTFILES/ablil.zsh-theme $HOME/.oh-my-zsh/custom/themes/ablil.zsh-theme
 
+# install custom zsh plugins
 if [[ ! -d $HOME/.oh-my-zsh/custom/plugins/one-character ]]; then
 	git clone https://github.com/ablil/one-character.git $HOME/.oh-my-zsh/custom/plugins/one-character
 fi
+
+# setup custom shell commands
+find $DOTFILES/scripts/bin/ -type f -executable -exec ln -sf {} $HOME/.local/bin/ \;
+
+
+# DONT FORGET TO
+echo "Do NOT forget to:"
+echo "pip3 install -r $DOTFILES/scripts/bin/requirements.txt"
