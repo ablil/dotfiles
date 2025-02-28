7ea0198e638f7a6c6fb9f3fd5477daca43d97c6d
ablil <ablil@proton.me>
2025-02-28 19:00:22 +0000

    Refactor: Improve Git workflow and remove unused GitHub Actions
    This commit refactors the Git workflow by moving the `.gitconfig` file to the `git` directory and improving the `setup.sh` script for easier management of Git hooks.  It also removes unused GitHub Actions related to Gemini AI review, Docker image publishing, and PyPI package deployment, simplifying the project's configuration.  A new release workflow has been added.

--- git/setup.sh
+++ git/setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-script_path=$(realpath $0)
-script_dir=$(dirname $script_path)
+set -e
 
-mkdir -p ~/.git-templates/hooks
-ln -sf $(find $script_dir -type f -exec realpath {} \; | xargs) ~/.git-templates/hooks/ 
+ln -sf $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
+
+# setup git repository template
+mkdir -p $HOME/.git-templates/hooks
+ln -sf $HOME/.dotfiles/git/hooks/{commit-msg,pre-commit,pre-push} $HOME/.git-templates/hooks/
 
 
+cat <<EOF
+make sure to:
+    * install Github cli
+    * set correct user.name and user.email
+EOF
