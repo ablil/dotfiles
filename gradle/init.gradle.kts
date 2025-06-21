4d80b1f820a843a8596c9afc04e578d90e3067f4
ablil <ablil@proton.me>
2025-06-21 11:55:53 +0100

    feat: shorten spring logging pattern

--- gradle/init.gradle.kts
+++ gradle/init.gradle.kts
@@ -1,24 +1,31 @@
+// package with DEBUG log level
+val loggingPackages = setOf(
+    "com.github.ablil",
+    "net.payback",
+    "org.springframework.security",
+    "org.springframework.web",
+)
+
+// custom spring properties
 val springProperties = setOf(
     "--spring.main.banner-mode=off",
     "--spring.output.ansi.enabled=always",
     "--logging.include-application-name=false",
-    "--logging.level.org.springframework.security=debug",
-    "--logging.level.net.payback=debug",
-    "--logging.level.com.github.ablil=debug",
-    "--logging.pattern.dateformat=HH:mm:ss.SSS"
+    "--logging.pattern.dateformat=HH:mm:ss.SSS",
+    "--logging.pattern.console=%clr(%d{HH:mm:ss.SSS}) %clr(%5p) %clr(%-40.40logger{39}){cyan} %m%n", // shorten log pattern
 )
 
 val springSystemProperties = mapOf(
-    "hibernate.generate_statistics" to "true",
-    "hibernate.show_sql" to "true",
-    "hibernate.format_sql" to "true"
+    "hibernate.generate_statistics" to "false",
+    "hibernate.show_sql" to "false",
+    "hibernate.format_sql" to "false"
 )
 
 allprojects {
     afterEvaluate {
         if (pluginManager.hasPlugin("org.springframework.boot") ) {
             tasks.named<JavaExec>("bootRun") {
-                args(springProperties)
+                args(springProperties + loggingPackages.map { "--logging.level.$it=debug" })
                 systemProperties(springSystemProperties)
             }
         }
